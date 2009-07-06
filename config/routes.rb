81313f2886f607a48310bed76f499f949b0bed72
mat_aki <matsumura.aki@gmail.com>
2009-07-06 15:00:35 +0900

    [#743] fix space

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller=>"pages", :action => "root"
+  map.root :controller => "pages", :action => "root"
 
   map.logout   '/logout',   :controller => 'sessions', :action => 'destroy'
   map.login    '/login',    :controller => 'sessions', :action => 'new'
